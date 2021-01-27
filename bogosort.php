dc8450e117be3abc12a1f3ac83ecd9b07236f695
FinKrise <42264884+FinKrise@users.noreply.github.com>
2021-01-27 11:26:19 +0200

    Update bogosort.php

--- bogosort.php
+++ bogosort.php
@@ -1,20 +1,19 @@
-
 <?php
-$pituus = 10;
+$pituus = 10; //Määritetään array listan arvojen määrä
 $i = 1; //alustetaan $i arvo foreach looppia varten
-$arvot = range(1, $pituus); //Määritetään arrayn pituus
+$arvot = range(1, $pituus); //Määritetään itse ArrayList
 
 shuffle($arvot); //sekoittaa arrayn sisäiset arvot
 
-foreach ($arvot as $num) {
-	echo "<!-- arvottu arvo $num-->";
+foreach ($arvot as $num) { //Foreach looppi käy array listan jokaisen arvon läpi, jossa $num vastaa yhtä arvoa listassa
+	echo "<!-- arvottu arvo $num-->"; //Jättää HTML koodiin kommentin, joka kertoo kyseisen arvon alkuperäisen luvun
 	
 	if ($num = $i) {
-		echo "$num "; //tulostaa kyseisen numeron jos se täsmää $i arvoa
+		echo "$num "; //tulostaa kyseisen luvun jos se täsmää $i arvoa. $i arvo nousee.
 		$i++;
 	} 
 	else { 
-		$num = rand(1, $pituus); //sekoittaa kyseisen numeron arraystä uudelleen
+		$num = rand(1, $pituus); //sekoittaa kyseisen numeron listassa uudelleen
 	}
 }
 ?>
